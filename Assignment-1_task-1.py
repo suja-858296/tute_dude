8818469fdf67f070ca5b4f3e69aeed4914a23c51
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-02-12 20:14:57 +0530

    first commit

---
+++ Assignment-1_task-1.py
@@ -0,0 +1,21 @@
+# Task 1: Perform Basic Mathematical Operations
+#  Problem Statement: Write a Python program that does the following:
+#  1.  Takes two numbers as input from the user.
+#  2.  Performs the basic mathematical operations on these two numbers:
+#  o	Addition
+#  o	Subtraction
+#  o	Multiplication
+#  o	Division
+#   3.  Displays the results of each operation on the screen.
+
+a = int(input("Enter a number"))
+b = int(input("Enter a number"))
+
+addition= (a + b)
+substraction = (a - b)
+Multiplication = (a * b)
+Division = (a / b)
+print(f"Addition:{addition}")
+print(f"Substraction:{substraction}")
+print(f"Multiplication:{Multiplication}")
+print(f"Division:{Division}")
---
+++ Assingment-1_task-2.py
@@ -0,0 +1,10 @@
+# Task 2: Create a Personalized Greeting
+#     Problem Statement: Write a Python program that:
+# 1.  Takes a user's first name and last name as input.
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name
+
+a = input("Enter First Name: ")
+b = input("Enter Last Name: ")
+
+print(f"Hello,{a}{b}! Welcome to the python program")
