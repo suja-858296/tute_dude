865a9422f5b90859b7352cd47cd03603ecd4d018
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-07-12 15:07:31 +0530

    Task-13 JS

---
+++ JavaScript/Task-13/script.js
@@ -0,0 +1,32 @@
+let num1 = 12;
+let num2 = 13;
+let num3 = 22;
+let num4 = 33;
+
+
+if(num1 % 2 ==0){
+    console.log(`The number ${num1} is Even`);
+}
+else{
+    console.log(`The number ${num1} is Odd`);
+}
+if(num2 % 2 ==0){
+    console.log(`The number ${num2} is Even`);
+}
+else{
+    console.log(`The number ${num2} is Odd`);
+}
+if(num3 % 2 ==0){
+    console.log(`The number ${num3} is Even`);
+}
+else{
+    console.log(`The number ${num3} is Odd`);
+}
+if(num4 % 2 ==0){
+    console.log(`The number ${num4} is Even`);
+}
+else{
+    console.log(`The number ${num4} is Odd`);
+}
+
+console.log("Thankyou :-)");
