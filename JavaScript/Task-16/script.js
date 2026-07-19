9bc1b72122daafa2f6706829d247cfba6cabcf7f
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-07-19 14:33:10 +0530

    Task-16 JS

--- JavaScript/Task-16/script.js
+++ JavaScript/Task-16/script.js
@@ -14,37 +14,43 @@ const studentGrid = document.getElementById("studentGrid")
 const searchInput = document.getElementById("studentSearch")
 const searchButton = document.getElementById("searchButton")
 
-function renderStudents(filteredStudents){
-    studentGrid.innerHTML= '';
 
-    if(filteredStudents.length ===0){
-        studentGrid.innerHTML`<p style=""grid-column: 1/-1; text-align: center; color:#888; padding:20px;>No matching Reords Found.</p> `;
+function renderStudents(filteredStudents) {
+    
+    if (filteredStudents.length === 0) {
+        studentGrid.innerHTML = `<p style="grid-column: 1/-1; text-align: center; color:#888;">No matching records found.</p>`;
         return;
     }
-    filteredStudents.forEach(student => {
-        const card = document.createElement('div');
-        card.className = 'student-card';
 
-        card.innerHTML= `
-        <p><strong>Student Name: </strong> ${student.Name}</p>
-        <p><strong>Marks: </strong> ${student.Marks}</p>
-        <p><strong>Class: </strong> ${student.Class}</p>
-        <p><strong>Address: </strong> ${student.Address}</p>
-        `;  
-        studentGrid.appendChild(card);
-    });
+   
+    const cardsHTML = filteredStudents.map(student => {
+        return `
+            <div class="student-card">
+                <p><strong>Student Name: </strong> ${student.Name}</p>
+                <p><strong>Marks: </strong> ${student.Marks}</p>
+                <p><strong>Class: </strong> ${student.Class}</p>
+                <p><strong>Address: </strong> ${student.Address}</p>
+            </div>
+        `;
+    }).join(''); 
+
+    
+    studentGrid.innerHTML = cardsHTML;
 }
-function performSearch(){
+
+function performSearch() {
     const query = searchInput.value.toLowerCase().trim();
-    const filtered = students.filter(student=> 
+    
+    const filtered = students.filter(student => 
         student.Name.toLowerCase().includes(query)
     );
+    
     renderStudents(filtered);
 }
-searchButton.addEventListener('click',performSearch);
-searchInput.addEventListener('keyup',(e)=> {
-    if(e.key === 'Enter'){
-        performSearch();
-    }
-});
-renderStudents(students)
+
+
+searchInput.addEventListener('input', performSearch);
+searchButton.addEventListener('click', performSearch);
+
+
+renderStudents(students);
