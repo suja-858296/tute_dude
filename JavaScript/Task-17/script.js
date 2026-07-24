ad4b73646eba618733c944b6a4d53eca21d4139e
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-07-24 13:36:43 +0530

    Task-17 JS' git commit -m Task-17 JS

--- JavaScript/Task-17/script.js
+++ JavaScript/Task-17/script.js
@@ -10,27 +10,28 @@
 
     });
  };
- console.log('Result');
+ console.log('Result of the divisions');
 
- const test= [
+ const num= [
     {a: 12 ,b:2},
     {a:30 , b:15},
     {a:20 , b:10},
     {a:50 , b:5},
     {a:100 ,b:0}    
  ];
-test.forEach(({a,b},index)=>{
+num.forEach(({a,b})=>{
     division(a,b)
         .then((result)=>{
-            console.log(`Test ${index+1} : ${a} / ${b} = ${result}`);
+            console.log(` ${a} / ${b} = ${result}`);
             
         })
-        .catch((Error)=> {
-            console.log(`Test ${index+1} : ${a} / ${0} -> Error: "The number can't be divided by zero";
-            }`);
+        .catch((error)=> {
+            console.log(` ${a} / ${0} -> Error: ${error.message}`);
+            
+            });
             
         });
-});
+
 
  
 
