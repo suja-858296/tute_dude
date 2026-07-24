40d6f39d0b261101a4845e7391b21a760ba9a9f0
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-07-24 09:42:05 +0530

    Task-17 JS

--- JavaScript/Task-17/script.js
+++ JavaScript/Task-17/script.js
@@ -1,23 +1,37 @@
-let num1 = 24;
-let num2 = 3;
-
-const division = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (num2 === 0) {
-            reject("Zere can't be divided by the first number ");
-        } else {
-            resolve("Success")
+ const division = (a,b)=> {
+    return new Promise((resolve, reject) => {
+        if(typeof a !=='number' || typeof b !=='number'){
+            reject(new error('The input must be a number'));
+       }else if (b ==0) {
+            reject('Division by zero is not allowed');
+        }else{
+            resolve(a/b);
         }
-    }, 3000);
-});
-division
-    .then((result) => {
-        console.log(num1 / num2)
-    })
-    .catch((error) => {
-        console.error(error)
-    });
 
+    });
+ };
+ console.log('--Promise Implementation--');
+
+ const test= [
+    {a: 10 ,b:2},
+    {a:30 , b:15},
+    {a:20 , b:10},
+    {a:50 , b:5},
+    {a:100 ,b:0}    
+ ];
+test.forEach(({a,b},index)=>{
+    division(a,b)
+        .then((result)=>{
+            console.log(`Test ${index+1} : ${a} / ${b} = ${result}`);
+            
+        })
+        .catch((error)=> {
+            console.log(`Test ${index+1} : ${a} / ${b} -> Error: ${error.message}`);
+            
+        });
+});
+
+ 
 
 
 
