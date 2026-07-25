808be1f9d1a6bc02c9fec3fbda5f464434e3ecaa
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-07-25 12:22:38 +0530

    Task-17 JS

--- JavaScript/Task-17/script.js
+++ JavaScript/Task-17/script.js
@@ -1,40 +1,31 @@
- const division = (a,b)=> {
+function division(a,b) {
     return new Promise((resolve, reject) => {
-        if(typeof a !=='number' || typeof b !=='number'){
-            reject(new Error('The input must be a number'));
-       }else if (b ==0) {
-            reject('Division by zero is not allowed');
-        }else{
-            resolve(a/b);
+        if (b == 0) {
+            reject("The number can't be divided by zero")
+        } else if  ( typeof b !== "number" || typeof a !=="number"  ){
+            reject("The input should be a number")
         }
+        else{
+            resolve(`The result is ${a}/${b} = ${a/b}`)
+        }
+    })
+}
 
-    });
- };
- console.log('Result of the divisions');
+numbers = [
+    {a: 20, b:10},
+    {a: 200, b:5},
+    {a: 330, b:3},
+    {a: "or", b:10},
+    {a: 200, b:0} //It is for checking zero
+]
 
- const num= [
-    {a: 12 ,b:2},
-    {a:30 , b:15},
-    {a:20 , b:10},
-    {a:50 , b:5},
-    {a:100 ,b:0},
-    {a:"or" , b:2}    
- ];
-num.forEach(({a,b})=>{
-    division(a,b)
-        .then((result)=>{
-            console.log(` ${a} / ${b} = ${result}`);
-            
-        })
-        .catch((error)=> {
-            console.log(` ${a} / ${b} -> Error: ${error.message}`);
-            
-            });
-            
-        });
-
-
- 
-
-
-
+numbers.forEach(element => {
+    division(element.a,element.b)
+    .then(message=>{
+        console.log(message)
+    })
+    .catch(error =>{
+        console.error(`Error is for a=${element.a}, b= ${element.b}:a${error}`);
+        
+    })
+});
