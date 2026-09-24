82908a77e84f1aa015de04d024bca2249a196a59
suja-858296 <sksujauddin@SKs-MacBook-Air.local>
2026-09-24 21:16:11 +0530

    Task-24

---
+++ React/Task-24 react/Task-23/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+
+export default App
